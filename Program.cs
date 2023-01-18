1af0f4260d8173d89f752908c8b91191cd4a1221
Kirill <kir.molokoff@yandex.ru>
2023-01-18 23:41:33 +0900

    fin

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Введите число:   ");
+string txtN = Console.ReadLine();
+int endNum = Convert.ToInt32(txtN);
+int num = 0;
+System.Console.WriteLine($"Чётные числа от 1 до {endNum}:");
+while (num < endNum)
+{
+    num++;
+    if (num % 2 == 0) System.Console.Write($"{num} ");
+}
+System.Console.WriteLine();
